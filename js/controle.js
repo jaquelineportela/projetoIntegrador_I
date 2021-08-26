042c1f24d55021d22ed364854d8df5f504806b20
Jaqueline Portela <jaquelineportelan@gmail.com>
2021-08-26 02:33:24 -0300

    controle js

--- js/controle.js
+++ js/controle.js
@@ -1,6 +1,12 @@
-const transactionUl = document.querySelector('#transactions')
+const transactionsUl = document.querySelector('#transactions')
+const incomeDisplay = document.querySelector('#money-plus ')
+const expenseDisplay = document.querySelector('#money-minus')
+const balanceDisplay = document.querySelector('#balance')
+const form = document.querySelector('#form')
+const inputTransactionName = document.querySelector('#text')
+const inputTransactionAmount = document.querySelector('amount')
 
-const DummyTransaction = [
+const dummyTransactions = [
     { id: 1, name:'Produto1', amount: -20},
     { id: 2, name:'salário2', amount: 300},
     { id: 3, name:'Produto3', amount: -10},
@@ -14,18 +20,57 @@ const addTransactionIntoDOM = transaction => {
     const li = document.createElement('li')
 
     li.classList.add(CSSClass)
-    li.innerHTML = '
+    li.innerHTML = `
     ${transaction.name} <span>${operator} R$ ${amountWithoutOperator} </span><button class "delete-btn"
-    '
-    transactionUl.append(li)
+    `
+    transactionsUl.append(li)
+}
+const updateBalanceValues = () => {
+    const transactionsAmounts = dummyTransactions
+        .map(transaction => transaction.amount)
+    const total = transactionsAmounts
+        .reduce((accumulator, transaction) => accumulator + transaction, 0).toFixed(2)
+    const income = transactionsAmounts
+        .filter(value => value > 0)
+        .reduce((accumulator, value) => accumulator + value, 0)
+        .toFixed(2)
+    const expense = Math.abs(transactionsAmounts)
+        .filter(value => value < 0)
+        .reduce((accumalator, value) => accumalator + value, 0)
+        .toFixed(2)
+    
+    balanceDisplay.textContent = `R$ ${total}`
+    incomeDisplay.textContent = `R$ ${income}`  
+    expenseDisplay.textContent = `R$ ${expense}`
+}
 
-    const updateBalanceValeus = () =>{
-        const transactionAmounts = DummyTransaction.map(transaction.amount)
-        const tatol = transactionAmounts.reduce((accumulador, transaction) => accumulador + transaction, 0)
-        console.log(transactionAmounts)
+const init = () => {
+    dummyTransactions.forEach(addTransactionIntoDOM)
+    updateBalanceValues()
+}
+init()
+
+const generateID = () => Math.round(Math.random() * 1000)
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const transactionName = inputTransactionName.value.trim()
+    const transactionAmount = inputTransactionAmount.value.trim()
+
+    if(transactionName === '' || transactionAmount === '') {
+        alert('Por favor, preencha os campos de nome e valor da transação')
+        return
     }
-} 
- const init = () =>{
-     DummyTransaction.forEach(addTransactionIntoDOM)
- }
+    const transaction = { 
+        id: generateID(),
+        name: transactionName,
+        amount: transactionAmount
+    }
+    dummyTransactions.push(transaction)
+    
     init()
+
+
+
+})
